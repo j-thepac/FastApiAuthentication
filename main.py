da27965ae98f721ef53889741e7b31640aa85bd8
j.thepac <j.thepac@gmail.com>
2021-07-31 06:13:24 -0400

    modifie code

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+The user types the username and password in the frontend, and hits Enter.
+The frontend sends that username and password to tokenUrl="token" ie., https://example.com/token
+The API checks that username and password, and responds with a "token"
+Normally, a token is set to expire after some time.
+The frontend stores that token temporarily somewhere.
+The user clicks in the frontend to go to another section of the frontend web app.
+So, to authenticate with our API, it sends a header Authorization with a value of Bearer plus the token.
+If the token contains foobar, the content of the Authorization header would be: Bearer foobar.
+
+
+"""
+
+
 
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status
@@ -12,7 +26,8 @@ db = {
         "disabled": False,
     }
 }
-
+# if your API was located at https://example.com/, then it would refer to https://example.com/token
+#call @app.post("/token") below
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class User(BaseModel):
@@ -29,7 +44,7 @@ async def validate_user(token: str = Depends(oauth2_scheme)):
     return user
 
 @app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+async def login(form_data: OAuth2PasswordRequestForm=Depends()):
     usn=form_data.username
     validate_user(usn)
     if "hashed"+form_data.password!= db[usn]["hashed_password"]:
@@ -40,5 +55,4 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
 async def read_users_me(current_user: User = Depends(validate_user)):
     return current_user
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+if __name__ == "__main__":uvicorn.run(app, host="0.0.0.0", port=9000)
